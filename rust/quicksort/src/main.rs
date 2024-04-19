e33344e75c1479e575e904350d50e0df07e3d9c8
Scott Rangeley <scott.rangeley@odds-bods.com>
2024-04-19 09:57:23 +0100

    further work on quick sort

--- rust/quicksort/src/main.rs
+++ rust/quicksort/src/main.rs
@@ -1,3 +1,14 @@
+use rand;
+
 fn main() {
-    println!("Hello, world!");
+    let array: [i8; 32] = rand::random();
+    println!("{:?}", array);
+
+}
+
+fn quicksort(array: Vec<i8>) -> Vec<i8> {
+    let pivotIndex = array.len();
+    let comparisonIndex = 0;
+    
+    array
 }

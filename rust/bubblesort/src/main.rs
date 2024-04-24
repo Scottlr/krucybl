1439e3e85928e255454f04fb756f579a085e9bfe
Scott Rangeley <scott.rangeley@odds-bods.com>
2024-04-24 12:23:22 +0100

    visualise sorting

--- rust/bubblesort/src/main.rs
+++ rust/bubblesort/src/main.rs
@@ -1,16 +1,19 @@
 use rand;
+use rgb::RGB8;
+use textplots::{ColorPlot, Chart, Shape};
+use std::{thread, time::Duration};
 
 fn main() {
     let array: [i8; 32] = rand::random();
-    println!("Unsorted: \n{:?}", array.to_vec());
-
     let sorted = bubble_sort(array.to_vec());
     println!("Sorted: \n{:?}", sorted)
+
+
 }
 
 fn bubble_sort(mut array: Vec<i8>) -> Vec<i8> {
     let max = array.len() - 1;
-    for index in 0..max {
+    for _ in 0..max {
         let mut swapped = false;
         for comparator in 0..(max) {
             if array[comparator] > array[comparator + 1] {
@@ -18,9 +21,27 @@ fn bubble_sort(mut array: Vec<i8>) -> Vec<i8> {
                 array.swap(comparator, comparator + 1)
             }
         }
+
+        visualise(&array);
+
         if !swapped {
             break;
         }
     }
     array
+}
+
+fn visualise(array: &Vec<i8>) {
+    std::process::Command::new("clear").status().unwrap();
+    let mut data = Vec::with_capacity(array.len());
+    for (index, value) in array.iter().enumerate() {
+        data.push((index as f32, *value as f32));
+    }
+
+    let mut chart = Chart::new(64, 60, 0.0, 32.0);
+    chart.linecolorplot(
+        &Shape::Bars(&data), 
+        RGB8 { r: 255, g: 0, b: 0 }
+    ).display();
+    thread::sleep(Duration::from_millis(333));
 }
--- rust/quicksort/src/main.rs
+++ rust/quicksort/src/main.rs
@@ -1,14 +1,17 @@
 use rand;
+use rgb::RGB8;
+use textplots::{ColorPlot, Chart, Shape};
+use std::{thread, time::Duration};
 
 fn main() {
-    let array: [i32; 32] = rand::random();
+    let array: [i8; 32] = rand::random();
     println!("Unsorted: \n{:?}", array);
 
     let sorted = quicksort(array.to_vec());
     println!("Sorted: \n{:?}", sorted)
 }
 
-fn quicksort(array: Vec<i32>) -> Vec<i32> {
+fn quicksort(array: Vec<i8>) -> Vec<i8> {
     if array.len() <= 1 {
         return array;
     }
@@ -17,16 +20,34 @@ fn quicksort(array: Vec<i32>) -> Vec<i32> {
 
     let less_sorted = quicksort(less);
     let more_sorted = quicksort(more);
+    let sorted = [less_sorted, vec![array[pivot]], more_sorted].concat();
 
-    [less_sorted, vec![array[pivot]], more_sorted].concat()
+    visualise(&sorted);
+
+    sorted
 }
 
-fn partition(array: Vec<i32>, pivot: usize) -> (Vec<i32>, Vec<i32>, usize) {
+fn partition(array: Vec<i8>, pivot: usize) -> (Vec<i8>, Vec<i8>, usize) {
     //create a ref to a slice of the array where we stop at the element before pivot.
     let scope = &array[0..pivot];
 
     //x is a reference to the original slice of the array, we need to then derefence it when using it.
-    let (less, more): (Vec<i32>, Vec<i32>) = scope.iter().partition(|&x| *x < array[pivot]);
+    let (less, more): (Vec<i8>, Vec<i8>) = scope.iter().partition(|&x| *x < array[pivot]);
 
     (less, more, pivot)
+}
+
+fn visualise(array: &Vec<i8>) {
+    std::process::Command::new("clear").status().unwrap();
+    let mut data = Vec::with_capacity(array.len());
+    for (index, value) in array.iter().enumerate() {
+        data.push((index as f32, *value as f32));
+    }
+
+    let mut chart = Chart::new(64, 60, 0.0, 32.0);
+    chart.linecolorplot(
+        &Shape::Bars(&data), 
+        RGB8 { r: 255, g: 0, b: 0 }
+    ).display();
+    thread::sleep(Duration::from_millis(333));
 }

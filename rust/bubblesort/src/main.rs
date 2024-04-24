c485c9aca08035d54d11268716de292e9d9b2a63
Scott Rangeley <scott.rangeley@odds-bods.com>
2024-04-24 11:33:14 +0100

    bubble sort

--- rust/bubblesort/src/main.rs
+++ rust/bubblesort/src/main.rs
@@ -1,9 +1,26 @@
 use rand;
 
 fn main() {
-    let array: [i32; 32] = rand::random();
-    println!("Unsorted: \n{:?}", array);
+    let array: [i8; 32] = rand::random();
+    println!("Unsorted: \n{:?}", array.to_vec());
 
-    let sorted = quicksort(array.to_vec());
+    let sorted = bubble_sort(array.to_vec());
     println!("Sorted: \n{:?}", sorted)
+}
+
+fn bubble_sort(mut array: Vec<i8>) -> Vec<i8> {
+    let max = array.len() - 1;
+    for index in 0..max {
+        let mut swapped = false;
+        for comparator in 0..(max) {
+            if array[comparator] > array[comparator + 1] {
+                swapped = true;
+                array.swap(comparator, comparator + 1)
+            }
+        }
+        if !swapped {
+            break;
+        }
+    }
+    array
 }
